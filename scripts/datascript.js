69f13936b663c5e15de7e7ece4471dd2d96be101
masimun <max.dekoninck@yahoo.com>
2015-04-18 12:04:25 +0200

    -upper graph

--- scripts/datascript.js
+++ scripts/datascript.js
@@ -93,9 +93,65 @@ $( function() {
     opts.tick_step =  24;
     opts.ticks = d3.time.years;
     // draw_graph('test', json, opts);
+    draw_upperGraph('First_War_Test', json, opts)
     draw_graph('First_War_Test', json, opts);
 
 } );
+
+function draw_upperGraph(name,data,our) {
+
+	var values = d3.range(1000).map(d3.random.bates(10));
+
+	var margin = {top: 10, right: 30, bottom: 30, left: 30},
+		width = 960 - margin.left - margin.right,
+		height = 500 - margin.top - margin.bottom;
+
+   var svg = d3.select("#upperGraph").append("svg")
+       .attr("width", width + margin.left + margin.right)
+       .attr("height", height + margin.top + margin.bottom)
+       .append("g")
+       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scale.linear()
+		.domain([0, 1])
+		.range([0, width]);
+
+	var data = d3.layout.histogram()
+		.bins(x.ticks(20))
+	(values);
+
+	var y = d3.scale.linear()
+		.domain([0, d3.max(data, function(d) { return d.y; })])
+		.range([height, 0]);
+
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom");
+
+	var bar = svg.selectAll(".bar")
+		.data(data)
+		.enter().append("g")
+		.attr("class", "bar")
+		.attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+	bar.append("rect")
+		.attr("x", 1)
+		.attr("width", x(data[0].dx) - 1)
+		.attr("height", function(d) { return height - y(d.y); });
+
+	bar.append("text")
+		.attr("dy", ".75em")
+		.attr("y", 6)
+		.attr("x", x(data[0].dx) / 2)
+		.attr("text-anchor", "middle")
+		.text(function(d) { return d.y; });
+
+	svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
+
+}
 
 
 
@@ -120,6 +176,8 @@ function draw_graph(name, data, our) {
 
     //data.forEach(logrow);
 
+
+    //console.log(data);
 
     var zScale = d3.scale.linear( ).domain(scaleExtent).rangeRound( [0, 1000] );
     var zSwitching = d3.scale.linear( ).domain([0,1000]).rangeRound([0,8]);
